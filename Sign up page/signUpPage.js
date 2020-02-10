bb98d82b16c6110e6fe7dd35fb9ed4765e1bd41e
danielarpapuc <emai aici>
2020-02-11 00:14:48 +0200

    wip"

--- Sign up page/signUpPage.js
+++ Sign up page/signUpPage.js
@@ -1,31 +1,34 @@
 document.getElementById("sign-up").addEventListener("click", createUserAccount);
 
-function createUserAccount (e) {
+function createUserAccount(e) {
     const userDatabase = [];
-const firstName = document.getElementById("first-name").value;
-const lastName = document.getElementById("last-name").value;
-const emailAddress = document.getElementById("email-address").value;
-const password = document.getElementById("password").value;
-const city = document.getElementById("city").value;
-const country = document.getElementById("country").value;
+    const firstName = document.getElementById("first-name").value;
+    const lastName = document.getElementById("last-name").value;
+    const emailAddress = document.getElementById("email-address").value;
+    const password = document.getElementById("password").value;
+    const city = document.getElementById("city").value;
+    const country = document.getElementById("country").value;
     e.preventDefault()
-const userAccount = {
-    firstName,
-    lastName,
-emailAddress,
-
-password,
-city, 
-country
-}
+    const userAccount = {
+        firstName,
+        lastName,
+        emailAddress,
+        password,
+        city,
+        country
+    }
 
     console.log(emailAddress.pattern)
 
-console.log(userAccount);
-userDatabase.push(userAccount);
-console.log(userDatabase)
-console.log(userAccount.firstName.length)
+    console.log(userAccount);
+    userDatabase.push(userAccount);
+    console.log(userDatabase)
+    console.log(userAccount.firstName.length)
 }
 
-
-
+document.getElementById("city").addEventListener("change", unlockCountryField);
+function unlockCountryField() {
+    document.getElementById("country").firstElementChild.textContent = "test"
+    document.getElementById("country").removeAttribute("disabled");
+    document.getElementById("country").style.backgroundColor = "white";
+}
