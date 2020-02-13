c9b22d0854b2f85b9c63df2298bd635882e2bec9
danielarpapuc <emai aici>
2020-02-13 20:19:52 +0200

    wip

--- Sign up page/signUpPage.js
+++ Sign up page/signUpPage.js
@@ -1,14 +1,61 @@
-document.getElementById("sign-up").addEventListener("click", createUserAccount);
 
-function createUserAccount(e) {
-    const userDatabase = [];
-    const firstNameValue = document.getElementById("first-name").value;
+const eyeIconButton = document.getElementById("eye-icon")
+eyeIconButton.addEventListener("click", showPassword);
+ function showPassword () {
+     const eyeIcon = document.getElementById("eyeIcon")
+    const passwordField = document.getElementById("password");
+    if (passwordField.getAttribute("type") === "password") {
+         passwordField.setAttribute("type", "text");
+         eyeIcon.classList.replace("fa-eye-slash", "fa-eye")
+         console.log(passwordField)
+    } else {
+         passwordField.setAttribute("type", "password");
+         eyeIcon.classList.replace("fa-eye", "fa-eye-slash")
+         console.log(passwordField)
+    }
+ }
+document.getElementById("city").addEventListener("change", unlockCountryField);
+function unlockCountryField() {
+    const countryField = document.getElementById("country");
+    countryField.firstElementChild.textContent = "Select country";
+    countryField.removeAttribute("disabled");
+   countryField.style.backgroundColor = "white";
+}
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("sign-up").addEventListener("click", createUserAccount);
+// class UserAccount {
+//     constructor (firstName, lastName, emailAddress, password, city, country) {
+//         this.firstName = firstName;
+//         this.lastName = lastName;
+//         this.emailAddress = emailAddress;
+//         this.password = password;
+//         this.city = city;
+//         this.country = country
+//     }
+//     bundleUserInfo (){
+//         console.log(`FN: ${this.firstName}, LN: ${this.lastName}, EA: ${this.emailAddress}, PW: ${this.password}, CI: ${this.city}, CO: ${this.country}`)
+//             }
+
+// }
+ const userDatabase = []; 
+function createUserAccount(e) {   
+         e.preventDefault(); 
+   const firstNameValue = document.getElementById("first-name").value;
     const lastNameValue = document.getElementById("last-name").value;
     const emailAddressValue = document.getElementById("email-address").value;
     const passwordValue = document.getElementById("password").value;
     const cityValue = document.getElementById("city").value;
     const countryValue = document.getElementById("country").value;
-    e.preventDefault()
     const userAccount = {
         firstName: firstNameValue,
         lastName: lastNameValue,
@@ -17,30 +64,10 @@ function createUserAccount(e) {
         city: cityValue,
         country: countryValue
     }
-
-    console.log(userAccount);
+// const newUser = new UserAccount(firstNameValue, lastNameValue, emailAddressValue, passwordValue, cityValue, countryValue)
+//     console.log(newUser.bundleUserInfo());
     userDatabase.push(userAccount);
     console.log(userDatabase)
-    console.log(userAccount.firstNameValue.length)
 }
 
-city.addEventListener("change", unlockCountryField);
-function unlockCountryField() {
-    countryValue.firstElementChild.textContent = "Select country";
-    countryValue.removeAttribute("disabled");
-   countryValue.style.backgroundColor = "white";
-}
-const eyeIcon = document.getElementById("eye-icon")
-eyeIcon.addEventListener("click", showPassword);
- function showPassword () {
-    console.log(passwordValue)
-    //  if(passwordValue.type = "password") {
-    //      console.log(passwordValue)
-    //  } 
-     passwordValue.removeAttribute ("type");
-         passwordValue.setAttribute("type", "text")
-     console.log(passwordValue)
-    //  else {
-    //      console.log("WOOOOW")
-    //  }
- }
+
