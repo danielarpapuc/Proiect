dc4965da27ccbe3e88fe24ec5419bb9a1c8d9ce8
danielarpapuc <emai aici>
2020-02-15 20:08:46 +0200

    wip

--- Landing page/header.js
+++ Landing page/header.js
@@ -5,19 +5,19 @@
 
 // const ;
 
-document.getElementById("second-header").addEventListener ("click", showMenu);
-function showMenu (e) {
-switch (e.target) {
-    case document.getElementById("button-1"):
-        document.getElementById("dropdown-1").classList.toggle("hide-list");
-        break;
-        case document.getElementById("button-2"):
-        document.getElementById("dropdown-2").classList.toggle("hide-list");
-        break; 
-        case document.getElementById("button-3"):
-            document.getElementById("dropdown-3").classList.toggle("hide-list");
-            break; 
-            case document.getElementById("button-4"):
-                document.getElementById("dropdown-4").classList.toggle("hide-list");
-}
-}
+// document.getElementById("second-header").addEventListener ("click", showMenu);
+// function showMenu (e) {
+// switch (e.target) {
+//     case document.getElementById("button-1"):
+//         document.getElementById("dropdown-1").classList.toggle("hide-list");
+//         break;
+//         case document.getElementById("button-2"):
+//         document.getElementById("dropdown-2").classList.toggle("hide-list");
+//         break; 
+//         case document.getElementById("button-3"):
+//             document.getElementById("dropdown-3").classList.toggle("hide-list");
+//             break; 
+//             case document.getElementById("button-4"):
+//                 document.getElementById("dropdown-4").classList.toggle("hide-list");
+// }
+// }
